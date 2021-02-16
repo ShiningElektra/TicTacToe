debfbf07d03958097483230df73d2b63ba1f5eee
ShiningElektra <elektrathanos@googlemail.com>
2021-02-16 10:55:35 +0100

    ♻️ declaring a Winner

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import styles from "../styles/Board.module.css";
 import Square from "./Square";
 
+// 9squares filled with null
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
+    // Wenn eine Auswahl getroffen wurde, kann der Inhalt nicht mehr verändert werden
+
+    // when click the squares can be filled with x or with o
     const newSquares = squares.slice();
     newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
@@ -18,7 +22,16 @@ export default function Board() {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   }
 
-  const status = "Nextplayer: " + (xIsNext ? "X" : "O");
+  // const status = "Nextplayer: " + (xIsNext ? "X" : "O");
+  const winner = calculateWinner(squares);
+  let status;
+  // wenn es einen gewinner gibt sind keine Züge mehr möglich
+  if (winner) {
+    status = "winner: " + winner;
+  } else {
+    // sonst führt er das aus
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   return (
     <div>
@@ -40,4 +53,25 @@ export default function Board() {
       </div>
     </div>
   );
+}
+function calculateWinner(squares) {
+  const lines = [
+    // alle möglichen Lösungen
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // wenn die linien drei gleiche squares beinhalten gibt es einen gewinner
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
